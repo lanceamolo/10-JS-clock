7658ddc3cc31262d235fc55779fa441ef17842d7
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-09-08 16:35:01 -0700

    update

---
+++ main.js
@@ -0,0 +1,26 @@
+function startTime() {
+  let today = new Date()
+  let h = today.getHours()
+  let amOrPm = h >= 12 ? "pm" : "am"
+  h = h % 12 || 12
+  let m = today.getMinutes()
+  let s = today.getSeconds()
+  h = checkHours(h)
+  m = checkTime(m)
+  s = checkTime(s)
+  document.getElementById("clock").innerHTML = h + ":" + m + ":" + s + amOrPm
+  let = setInterval(startTime, 1000)
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i
+  }
+  return i
+}
+
+function checkHours(i) {
+  if (i <= 9) {
+    i = "0" + i
+  }
+  return i
+}
